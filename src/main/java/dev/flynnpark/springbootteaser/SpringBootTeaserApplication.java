41a9d073f61675b4b3582fe987b9d13606d436c3
Flynn <wphestiraid@gmail.com>
2023-04-24 10:17:15 +0900

    :tada:  스프링부트 프로젝트 생성

---
+++ src/main/java/dev/flynnpark/springbootteaser/SpringBootTeaserApplication.java
@@ -0,0 +1,13 @@
+package dev.flynnpark.springbootteaser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTeaserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTeaserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/flynnpark/springbootteaser/SpringBootTeaserApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.flynnpark.springbootteaser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTeaserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
