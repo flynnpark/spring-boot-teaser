246d44fbfc8dd543048b7c39fcb7a15c8368e2f6
Flynn <wphestiraid@gmail.com>
2023-05-02 09:20:52 +0900

    :sparkles: 컴포넌트 스캔과 자동 의존관계 설정

---
+++ src/main/java/dev/flynnpark/springbootteaser/controllers/MemberController.java
@@ -0,0 +1,15 @@
+package dev.flynnpark.springbootteaser.controllers;
+
+import dev.flynnpark.springbootteaser.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
--- src/main/java/dev/flynnpark/springbootteaser/service/MemberService.java
+++ src/main/java/dev/flynnpark/springbootteaser/service/MemberService.java
@@ -2,13 +2,17 @@ package dev.flynnpark.springbootteaser.service;
 
 import dev.flynnpark.springbootteaser.domain.Member;
 import dev.flynnpark.springbootteaser.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
