699a948b90db42ab20f2ab5a8b37ae86e537bf0f
Flynn <wphestiraid@gmail.com>
2023-05-07 15:57:55 +0900

    :sparkles: 회원 웹 기능 - 등록

---
+++ src/main/java/dev/flynnpark/springbootteaser/controllers/MemberForm.java
@@ -0,0 +1,13 @@
+package dev.flynnpark.springbootteaser.controllers;
+
+public class MemberForm {
+    public String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
