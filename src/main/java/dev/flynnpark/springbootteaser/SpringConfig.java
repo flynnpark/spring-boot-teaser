c3f58a4c9ad37c1f9bd6592095bcc884d226eb0c
Flynn <wphestiraid@gmail.com>
2023-05-18 09:17:23 +0900

    :sparkles: 스프링 데이터 JPA

--- src/main/java/dev/flynnpark/springbootteaser/SpringConfig.java
+++ src/main/java/dev/flynnpark/springbootteaser/SpringConfig.java
@@ -1,33 +1,23 @@
 package dev.flynnpark.springbootteaser;
 
-import dev.flynnpark.springbootteaser.repository.JpaMemberRepository;
 import dev.flynnpark.springbootteaser.repository.MemberRepository;
 import dev.flynnpark.springbootteaser.service.MemberService;
-import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
-
-    private final EntityManager em;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
-
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-        return new JpaMemberRepository(em);
-    }
 }
